61b470e5d6ba876ef04f391b6b63e1297fa25491
Sanjan Geet Singh <sanjangs976@gmail.com>
2020-02-11 14:54:57 +0530

    Added two new instructions - exit, exitr

--- doc/Getting Started.md
+++ doc/Getting Started.md
@@ -1,1 +1,8 @@
 # Getting Started
+
+### List of Instructions
+
+Mnemonic | Opcode | IN | OUT | Description
+---------|--------|----|-----|------------
+EXIT | 0 | | | Exits the program with return code 0
+EXITR | 1 | 1: r_code | | Exits the program with return code ```r_code```
--- src/vm.hpp
+++ src/vm.hpp
@@ -8,7 +8,28 @@
 #ifndef __INFINIUMVM_VM_HPP__
 #define __INFINIUMVM_VM_HPP__
 
+enum Opcodes {  // List of Instructions
+    EXIT,   // Normally terminates the program.
+    EXITR   // Exits the program with a specific return code.
+};
+
 void execute(char* code, int lenght) {
+    int pos = 0;    // Current position in code.
+
+    while (pos < lenght) {  // Check if we have reached the end or not.
+        switch(code[pos]) {
+
+            case EXIT:
+                exit(0);
+
+            case EXITR:
+                int r_code = int(code[pos]);
+                exit(r_code);
+
+        }
+
+        pos++;  // Increment position in code.
+    }
 }
 
 #endif /* __INFINIUMVM_VM_HPP__ */
